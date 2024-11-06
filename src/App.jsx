6a4944247cef40b397345e513149f71dd938b538
sanjaydas <sdas101@gmail.com>
2024-11-06 13:21:08 +0530

    Completed-Initial-Pages-and- Routing-Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from './pages/About';
+import Profile from "./pages/Profile"
 
 const App = () => {
   return (
-    <div className='text-yellow-300'>App</div>
+    
+       <BrowserRouter>
+          <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/signin' element={<Signin />}/>
+               <Route path='/signup' element={<Signup />}/>
+               <Route path='/about' element={<About />}/>
+               <Route path='/profile' element={<Profile />}/>
+               
+          </Routes>
+       </BrowserRouter>
   )
 }
 
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        1<h1>SignUp</h1>
+    </div>
+  )
+}
+
+export default Signup
