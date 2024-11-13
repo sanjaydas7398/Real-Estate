4ac4b31f2165d5f59461515464267262a3405285
sanjay <sdas69101@gmail.com>
2024-11-13 14:57:20 +0530

    Completed database connection with MongoDB

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = 4000
-// console.log("Mongo URI:", process.env.MONGO);
 
 mongoose.connect(process.env.MONGO).then(()=>{console.log("db connection succesfully")}).catch((error)=>{console.log(error)})
 
